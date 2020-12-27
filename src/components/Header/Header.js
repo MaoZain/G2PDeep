5a2dfeae08e4260795b071b85c9c3fd5f99754b9
Shuai Zeng <zengs@umsystem.edu>
2020-12-27 08:06:47 -0600

    Replacing logo with G2PDeep logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Style from "./header.module.css";
-import logo from './favicon.ico'
+import logo from './G2PDeep_logo.png'
 
 export default class Header extends Component {
     render() {
@@ -9,9 +9,9 @@ export default class Header extends Component {
                 <div className={Style.row}>
                     <div className={Style.symbol}>
                         <h1>
-                            <a href='###' className={Style.logo}>
+                            <a href='/' className={Style.logo}>
                                 <img alt="logo" src={logo}/>
-                                Geno-Pheno-type Predictor
+                                Quantitative phenotypes prediction and genotype markers discovery
                             </a>
                         </h1>
                     </div>
