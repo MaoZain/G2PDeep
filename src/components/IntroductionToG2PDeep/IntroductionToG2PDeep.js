ec05206ed56af65a819a108310b3b4918d5b38b7
Shuai Zeng <zengs@umsystem.edu>
2021-01-03 21:47:52 -0600

    Modified introduction view size.

--- src/components/IntroductionToG2PDeep/IntroductionToG2PDeep.js
+++ src/components/IntroductionToG2PDeep/IntroductionToG2PDeep.js
@@ -63,11 +63,11 @@ export default class InputData extends Component {
           <img src={workflow_intro} style={{ width: '100%' }}></img>
           <Title level={4} style={{ paddingTop: '40px' }} >Welcome to G2PDeep</Title>
           <p>
-          Genomic selection uses single-nucleotide polymorphisms (SNPs) to predict quantitative phenotypes for enhancing traits in breeding populations and has been widely used to increase breeding efficiency for plants and animals. 
-          Existing statistical methods rely on a prior distribution assumption of imputed genotype effects, which may not fit experimental datasets. Emerging deep learning technology could serve as a powerful machine learning tool to predict quantitative phenotypes without imputation and also to discover potential associated genotype markers efficiently. 
-          We propose a deep-learning framework using convolutional neural networks (CNNs) to predict the quantitative traits from SNPs and also to investigate genotype contributions to the trait using saliency maps. 
-          The missing values of SNPs are treated as a new genotype for the input of the deep learning model. 
-          We tested our framework on experimental datasets of soybean. 
+            Genomic selection uses single-nucleotide polymorphisms (SNPs) to predict quantitative phenotypes for enhancing traits in breeding populations and has been widely used to increase breeding efficiency for plants and animals.
+            Existing statistical methods rely on a prior distribution assumption of imputed genotype effects, which may not fit experimental datasets. Emerging deep learning technology could serve as a powerful machine learning tool to predict quantitative phenotypes without imputation and also to discover potential associated genotype markers efficiently.
+            We propose a deep-learning framework using convolutional neural networks (CNNs) to predict the quantitative traits from SNPs and also to investigate genotype contributions to the trait using saliency maps.
+            The missing values of SNPs are treated as a new genotype for the input of the deep learning model.
+            We tested our framework on experimental datasets of soybean.
           </p>
           <p>
             G2PDeep is a web server for phenotype prediction and genotype markers discovery.
@@ -97,7 +97,7 @@ export default class InputData extends Component {
           <p>
             Users are able to create dataset on G2PDeep by a link to their owned SNP data. The dataset is visible only for the dataset owner.
             To transfer large-scale SNP dataset quickly and safely, the G2PDeep parses and transfers the dataset by the link provided by users.
-            The created dataset is divided into training dataset and validation dataset automatically by G2PDeep, where users are able to determine the ratio between them. 
+            The created dataset is divided into training dataset and validation dataset automatically by G2PDeep, where users are able to determine the ratio between them.
           </p>
           <p>Followings are restrictions to create a dataset:
           <ul>
@@ -116,23 +116,23 @@ export default class InputData extends Component {
           <img src={workflow_experiment} style={{ width: '100%' }}></img>
           <Title level={4} style={{ paddingTop: '40px' }} >Experiments</Title>
           <p>
-          G2PDeep allows users to build, train and analysis state-of-the-art deep learning models. 
-          It also provides a series of interactive charts to monitor the training processing and learning curve of the model. 
-          To get the best performance of the model for further use, G2PDeep also provides comparisons among models by showing summary of model parameters and chart metrics. 
-          All details of datasets and models can be retrieved by users. 
-          G2PDeep takes 50 minutes in average to train a customized model with 4000 training samples in 1000 epochs.
+            G2PDeep allows users to build, train and analysis state-of-the-art deep learning models.
+            It also provides a series of interactive charts to monitor the training processing and learning curve of the model.
+            To get the best performance of the model for further use, G2PDeep also provides comparisons among models by showing summary of model parameters and chart metrics.
+            All details of datasets and models can be retrieved by users.
+            G2PDeep takes 50 minutes in average to train a customized model with 4000 training samples in 1000 epochs.
           </p>
         </div>
         <div id='step_content_prediction' style={{ display: this.state.current_step == 3 ? 'block' : 'none' }}>
           <img src={workflow_prediction} style={{ width: '100%' }}></img>
           <Title level={4} style={{ paddingTop: '40px' }} >Prediction & discovery</Title>
           <p>
-          Users can paste their genotype from csv file to input area for prediction purpose only. 
-          G2PDeep currently includes SoyNAM dataset and pre-trained models by this dataset. 
-          Users are able to customize and train a model with this publicly available dataset and predict phenotype using the pre-trained models directly. 
-          Once the prediction is done, a bar chart appears to show predicted quantitative trait values. 
-          Saliency map approach is performed to measure the differences between SNPs and the traits. 
-          All results are downloadable.
+            Users can paste their genotype from csv file to input area for prediction purpose only.
+            G2PDeep currently includes SoyNAM dataset and pre-trained models by this dataset.
+            Users are able to customize and train a model with this publicly available dataset and predict phenotype using the pre-trained models directly.
+            Once the prediction is done, a bar chart appears to show predicted quantitative trait values.
+            Saliency map approach is performed to measure the differences between SNPs and the traits.
+            All results are downloadable.
           </p>
         </div>
       </div>
@@ -142,8 +142,8 @@ export default class InputData extends Component {
       <div>
         <img src={deep_gwas_img} style={{ width: '80%' }}></img>
         <p>
-        For SoyNAM dataset, the our method (dualCNN) reaches a higher PCC than the other four statistical models and was less affected by the training population size in low heritability traits as yield, moisture, and protein. 
-        As long as the training size reached 1,500, our model showed a higher performance than statistical models.
+          For SoyNAM dataset, the our method (dualCNN) reaches a higher PCC than the other four statistical models and was less affected by the training population size in low heritability traits as yield, moisture, and protein.
+          As long as the training size reached 1,500, our model showed a higher performance than statistical models.
         </p>
       </div>
     )
@@ -152,10 +152,10 @@ export default class InputData extends Component {
       <div>
         <img src={deep_gwas_img} style={{ width: '80%' }}></img>
         <p>
-        Genotypes are one-hot coded and passed to the input processing block, which contains two streams of CNNs. 
-        The first stacked-CNN stream contains two feed-forward CNN layers with kernel sizes 4 and 20. 
-        The second single-CNN stream contains one CNN layer with kernel size 4, followed by an add-up layer to aggregate outputs from the two streams. 
-        The feature processing block contains another single convolution layer with kernel size 4. Processed features are then passed to the output processing block, which contains a flatten layer and a fully connected dense layer.
+          Genotypes are one-hot coded and passed to the input processing block, which contains two streams of CNNs.
+          The first stacked-CNN stream contains two feed-forward CNN layers with kernel sizes 4 and 20.
+          The second single-CNN stream contains one CNN layer with kernel size 4, followed by an add-up layer to aggregate outputs from the two streams.
+          The feature processing block contains another single convolution layer with kernel size 4. Processed features are then passed to the output processing block, which contains a flatten layer and a fully connected dense layer.
         </p>
       </div>
     )
@@ -167,14 +167,16 @@ export default class InputData extends Component {
         <div>
 
           <Title level={3}>Quick start</Title>
-          <Row>
-            <Col span={16}>
-              {step_comp}
-              {step_content}
+          <div style={{ width: '850px' }}>
+            <Row>
+              <Col>
+                {step_comp}
+                {step_content}
 
-            </Col>
+              </Col>
 
-          </Row>
+            </Row>
+          </div>
         </div>
 
       </div>
