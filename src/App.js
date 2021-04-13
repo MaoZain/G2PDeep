fbfc8f27db87d4d2a892ef88be41f3a0984c2966
Shuai Zeng <zengs@umsystem.edu>
2021-04-13 06:02:25 -0500

    modified docker compose and app

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends React.PureComponent {
   }
 
   componentDidMount = () => {
-    if(!localStorage.getItem('G2PDeep')){
+    if(localStorage.getItem('G2PDeep')){
       this.setUserId();
     }
     // this.setUserId();
